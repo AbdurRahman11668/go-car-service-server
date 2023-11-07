e3c72e95ecca43430140c8aa9712962fa9634a9e
Abdur Rahman <abdur1222.ar@gmail.com>
2023-11-07 15:33:39 +0600

    serviceDetails DEscription added through the id, took data from mongodb

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:5173',
+//       // "https://cars-doctor-6c129.web.app",
+//       // "https://cars-doctor-6c129.firebaseapp.com",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@gocarservice.4k8igdd.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,16 +29,52 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+// middlewares
+// const logger = (req, res, next) => {
+//   console.log("log: info", req.method, req.url);
+//   next();
+// };
+
+// const verifyToken = (req, res, next) => {
+//   const token = req?.cookies?.token;
+//   if (!token) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized access" });
+//     }
+//     req.user = decoded;
+//     next();
+//   });
+// };
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-
     const carCollection = client.db("carDB").collection("products");
+
+    // auth related api
+    // app.post("/jwt", logger, async (req, res) => {
+    //   const user = req.body;
+    //   console.log("user for token", user);
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1h",
+    //   });
+
+    //   res
+    //     .cookie("token", token, {
+    //       httpOnly: true,
+    //       secure: true,
+    //       sameSite: "none",
+    //     })
+    //     .send({ success: true });
+    // });
 
     app.get("/products", async (req, res) => {
       const cursor = carCollection.find();
@@ -36,9 +82,18 @@ async function run() {
       res.send(result);
     });
 
+  app.get("/products/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await carCollection.findOne(query);
+    res.send(result);
+  });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -46,10 +101,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Fitness Club Center server is running.....')
-})
-  
-  app.listen(port, () => {
-    console.log(`Fitness Server is running on port: ${port}`);
-  });
+app.get("/", (req, res) => {
+  res.send("Fitness Club Center server is running.....");
+});
+
+app.listen(port, () => {
+  console.log(`Fitness Server is running on port: ${port}`);
+});
