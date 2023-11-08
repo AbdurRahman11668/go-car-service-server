068ff366ca90f4174b4e9cbf0b13da81e886eb5c
Abdur Rahman <abdur1222.ar@gmail.com>
2023-11-08 07:14:27 +0600

    used jwt in my service page to show the products

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-// app.use(
-//   cors({
-//     origin: [
-//       'http://localhost:5173',
-//       // "https://cars-doctor-6c129.web.app",
-//       // "https://cars-doctor-6c129.firebaseapp.com",
-//     ],
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", "http://localhost:5174",
+      // "https://cars-doctor-6c129.web.app",
+      // "https://cars-doctor-6c129.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+// app.use(cors());
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@gocarservice.4k8igdd.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,24 +34,24 @@ const client = new MongoClient(uri, {
 });
 
 // middlewares
-// const logger = (req, res, next) => {
-//   console.log("log: info", req.method, req.url);
-//   next();
-// };
+const logger = (req, res, next) => {
+  console.log("log: info", req.method, req.url);
+  next();
+};
 
-// const verifyToken = (req, res, next) => {
-//   const token = req?.cookies?.token;
-//   if (!token) {
-//     return res.status(401).send({ message: "unauthorized access" });
-//   }
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).send({ message: "unauthorized access" });
-//     }
-//     req.user = decoded;
-//     next();
-//   });
-// };
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -60,7 +61,24 @@ async function run() {
     const carCollection = client.db("carDB").collection("products");
     const purchaseCollection = client.db("carDB").collection("purchases");
 
+    // Auth related api
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
 
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    // Service Api
     app.get("/products", async (req, res) => {
       const cursor = carCollection.find();
       const result = await cursor.toArray();
@@ -73,6 +91,23 @@ async function run() {
     const result = await carCollection.findOne(query);
     res.send(result);
   });
+
+  app.get("/products", logger, verifyToken, async (req, res) => {
+    console.log(req.query.email);
+    // console.log('ttttt token', req.cookies.token)
+    console.log("user in the valid token", req.user);
+    if (req.query.email !== req.user.email) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+
+    let query = {};
+    if (req.query?.email) {
+      query = { email: req.query.email };
+    }
+    const result = await carCollection.find(query).toArray();
+    res.send(result);
+  });
+
 
   app.post("/products", async (req, res) => {
     const product = req.body;
